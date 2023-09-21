4be4e09ffd9f1e02dcf91ebb7f8c1e94986d1209
kichanll <kaipengxiejiebin@126.com>
2023-09-21 14:10:42 +0800

    Update nvidia_docker_install.sh

--- nvidia_docker_install.sh
+++ nvidia_docker_install.sh
@@ -1,8 +1,16 @@
+#nvidia-docker2安装：
+#https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#docker
+
+sudo apt-get update
+sudo apt-get install -y curl
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu18.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
-docker run --runtime=nvidia --rm nvidia/cuda:11.0-devel nvidia-smi
+sudo chmod o+rw /var/run/docker.sock
+sudo docker run --runtime=nvidia --rm --gpus all nvidia/cuda:11.0.3-runtime-ubuntu20.04 nvidia-smi
+#sudo docker run --runtime=nvidia --rm --gpus all nvidia/cuda:12.2.0-runtime-ubuntu20.04 nvidia-smi
+
 
 
 # extra operation
@@ -21,4 +29,4 @@ docker run --runtime=nvidia --rm nvidia/cuda:11.0-devel nvidia-smi
 #    "default-runtime": "nvidia"
 #}
 
-#docker run -it --name xiejiebin01_tts -p 50001:50001 -p 50002:50002 -p 50003:50003 -p 50004:50004 --mount type=bind,source=/home/xiejiebin01,target=/home nvidia/cuda:11.0-devel
+#docker run -it --gpus all --name xiejiebin01_tts -p 50001:50001 -p 50002:50002 -p 50003:50003 -p 50004:50004 --mount type=bind,source=/home/xiejiebin01,target=/home nvidia/cuda:11.0.3-runtime-ubuntu20.04
